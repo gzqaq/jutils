ae0de40f79371ee75288975316bf4697a316d900
Ziqin Gong <gongzq0301@sjtu.edu.cn>
2023-10-04 14:12:19 +0800

    Add more types and imported modules

--- jutils/__init__.py
+++ jutils/__init__.py
@@ -1,9 +1,9 @@
-from .types import Array, PRNGKey, DType, Shape
+from .types import Array, PRNGKey, DType, Shape, VariableDict, DataDict, Initializer, ApplyFunction
 from .random import default_rng, split_rng, rng_wrapper
 from .operators import flatten, matinv, bop, bdot, bmm, norm, sum_of_square
 
 import jax
-from jax import jit, vmap, random
+from jax import lax, jit, vmap, random
 from jax import numpy as np
 
 __version__ = "0.1.0"
--- jutils/types.py
+++ jutils/types.py
@@ -1,6 +1,14 @@
 import chex
+from flax.core.frozen_dict import FrozenDict
+from typing import Callable, Any, Dict, Tuple, Union
 
 Array = chex.Array
 PRNGKey = chex.PRNGKey
 DType = chex.ArrayDType
 Shape = chex.Shape
+
+VariableDict = Union[FrozenDict[str, Any], Dict[str, Any]]
+DataDict = Union[FrozenDict[str, Array], Dict[str, Array]]
+
+Initializer = Callable[[PRNGKey, Shape, DType], Any]
+ApplyFunction = Callable[[VariableDict, Any], Any]
