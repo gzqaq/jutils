ece9fcfb33963fab3235f3d1adf13e296c34c231
Ziqin Gong <gongzq0301@sjtu.edu.cn>
2023-10-23 11:00:10 +0800

    Format code

--- jutils/utils.py
+++ jutils/utils.py
@@ -12,19 +12,23 @@ def flatten(a: Array, axis: int = -1) -> Array:
   """Flatten ndarray starting from the given axis."""
   return a.reshape(-1, *a.shape[axis:])
 
+
 @partial(jit, static_argnames=("axis", "size"))
 def tile_over_axis(a: Array, size: int, axis: int = 0) -> Array:
   return np.repeat(np.expand_dims(a, axis), size, axis=axis)
+
 
 @jit
 def concat(*args) -> Array:
   """Flatten all ndarrays and concatenate them. Like c() in R."""
   return np.concatenate(args, axis=None)
 
+
 @jit
 def cbind(*args) -> Array:
   """Concatenate args along the last axis. Like cbind() in R."""
   return np.concatenate(args, axis=-1)
+
 
 def type2dtype(t):
   if t == int:
@@ -36,9 +40,11 @@ def type2dtype(t):
   else:
     raise ValueError(f"Unsupported type: {t}")
 
+
 def scalar2jax(scalar: Scalar):
   chex.assert_scalar(scalar)
   return np.asarray(scalar, dtype=type2dtype(type(scalar)))
+
 
 def make_supervised_train(n_epochs: int, bs: int, loss_fn):
   def train(rng, state, inp, labels):
@@ -53,12 +59,15 @@ def make_supervised_train(n_epochs: int, bs: int, loss_fn):
 
       rng, state, inp, labels = run_state
       rng, inds = rng_wrapper(random.permutation)(rng, np.arange(ds_size))
-      shuffled = tree_map(lambda x: x[inds].reshape(ds_size // bs, bs, *x.shape[1:]), (inp, labels))
+      shuffled = tree_map(
+          lambda x: x[inds].reshape(ds_size // bs, bs, *x.shape[1:]),
+          (inp, labels))
       state, losses = lax.scan(one_batch, state, shuffled)
 
       return (rng, state, inp, labels), losses
 
-    (_, state, *_), losses = lax.scan(one_epoch, (rng, state, inp, labels), None, n_epochs)
+    (_, state, *_), losses = lax.scan(one_epoch, (rng, state, inp, labels),
+                                      None, n_epochs)
     return state, losses
 
   return jit(train)
